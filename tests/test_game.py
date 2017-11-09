432d2cc81d5ef4b1328568a49ac7485cce2f2f3a
Raisa <raisa.abzelimova@yandex.ru>
2017-11-09 17:11:31 +0300

    please, work

--- tests/test_game.py
+++ tests/test_game.py
@@ -10,3 +10,18 @@ def test_add_round():
 
     test_game.add_round(test_round,test_player_first,test_player_second)
     assert test_game.won_rounds[test_player_second.id] == 2
+
+def test_get_winner():
+    from hangman.game import Game
+    from hangman.round import Round
+    from hangman.players import HumanPlayer
+
+    test_game = Game()
+    test_round = Round('python')
+    test_player_first = HumanPlayer()
+    test_player_second = HumanPlayer()
+
+    test_game.add_round(test_round, test_player_first, test_player_second)
+    test_game.add_round(test_round, test_player_first, test_player_second)
+
+    assert str(test_game.get_winner()) == str(test_player_first.id)
