f862a5e7e1b04dbbb79339c5e39f9f8ab7dc7529
Raisa <raisa.abzelimova@yandex.ru>
2017-11-09 14:40:08 +0300

    please, work

--- tests/test_players.py
+++ tests/test_players.py
@@ -5,3 +5,12 @@ def test_players():
     test_players = HumanPlayer(None)
 
     assert test_players._validate_word() == False
+
+def test_quess_new_word():
+    from hangman.players import HumanPlayer
+
+    value = 'Input your English word: '
+
+    test_quess_new_word = HumanPlayer('работай')
+
+    assert test_quess_new_word().quess_new_word() == value
