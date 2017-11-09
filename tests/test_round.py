be6ddcb70b38097c7ceef010db07d88898910ba3
Raisa <raisa.abzelimova@yandex.ru>
2017-11-09 14:57:47 +0300

    please, work

--- tests/test_round.py
+++ tests/test_round.py
@@ -5,3 +5,11 @@ def test_round():
     test_round = Round('python')
 
     assert test_round.is_word_solved() == False
+
+def test_word_is_solved():
+    from hangman.round import Round
+
+    test_round = Round('python')
+    test_round.word_status = [True for _ in test_round.word]
+
+    assert test_round.is_word_solved() is True
